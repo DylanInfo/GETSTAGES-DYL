8f2d9a8c4d945f3f36783cfbf0d1a79f3ebcf674
DylanInfo <dylan.simplon@gmail.com>
2024-06-14 10:58:53 +0200

    Update f_bdd.php

--- include/function/f_bdd.php
+++ include/function/f_bdd.php
@@ -1,18 +1,30 @@
-﻿<?php
+`﻿<?php
 
 function connexionBDD()
 {
-	try
-	{
-            $bdd = new PDO('mysql:host=servbd;port=3306;dbname=bdd_geststages;charset=utf8', 'usergs', 'mdpGS', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    try
+    {
+        $options = array(
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::MYSQL_ATTR_SSL_CA => 'DigiCertGlobalRootCA.crt.pem' // Chemin vers le certificat RootCA de Azure
+            );
+    
+            $bdd = new PDO(
+            'mysql:host=bdd-getstage-dyll.mysql.database.azure.com;port=3306;dbname=bdd_geststages;charset=utf8',
+            'wazo',
+            '18022000Dragau&',
+            $options
+        );
+            //array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+        
             return $bdd;
-	}
-	catch(Exception $e)
-	{
-		$pdo_error = $e->getMessage();
+    }
+    catch(Exception $e)
+    {
+        $pdo_error = $e->getMessage();
                 return false;
-	}
+    }
     
 }
 
-?>
+?>`
